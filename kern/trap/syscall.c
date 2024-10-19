9ef9243b2540fd4eca88429f54a498cd883be38f
AhmedMaged <osans026@gmail.com>
2024-10-20 01:22:26 +0300

    validating the params in system calls , passed the test case

--- kern/trap/syscall.c
+++ kern/trap/syscall.c
@@ -316,15 +316,19 @@ void sys_allocate_user_mem(uint32 virtual_address, uint32 size)
 {
 	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
 
-	allocate_user_mem(cur_env, virtual_address, size);
+	if(virtual_address+size<USER_HEAP_START || virtual_address+size>USER_HEAP_MAX)
+		env_exit();
+
+	else allocate_user_mem(cur_env, virtual_address, size);
 	return;
 }
 
 void sys_allocate_chunk(uint32 virtual_address, uint32 size, uint32 perms)
 {
 	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
-
-	allocate_chunk(cur_env->env_page_directory, virtual_address, size, perms);
+	if(virtual_address+size<USER_HEAP_START || virtual_address+size>USER_HEAP_MAX)
+			env_exit();
+	else allocate_chunk(cur_env->env_page_directory, virtual_address, size, perms);
 	return;
 }
 
