22f86e933155be684561f45bb40ad89f64fd7513
adham667 <adhamsaeed667@gmail.com>
2024-10-23 23:38:33 +0300

    added some tests in comments

--- lib/dynamic_allocator.c
+++ lib/dynamic_allocator.c
@@ -303,6 +303,10 @@ void free_block(void *va)
 	if(va==NULL){
 		return;
 	}
+	int8 is_myblock_free = is_free_block(va);
+	if(is_myblock_free){
+		return;
+	}
 	//some data for my block
 	struct BlockElement * my_block_ptr = (struct BlockElement *) va;
 	uint32 my_block_size = get_block_size(va);
@@ -384,9 +388,12 @@ void *realloc_block_FF(void* va, uint32 new_size)
 			struct BlockElement * next_block_ptr = (struct BlockElement *) next_block;
 
 			// merge without splitting
+			//this happens in two cases this and if 0<= (next_block_size+old_size)-new_size < 16
 			if (next_block_size+old_size == new_size)
 			{
+
 				LIST_REMOVE(&freeBlocksList, next_block_ptr);
+				//set_block_data(va, next_block_size+old_size, 1);
 				set_block_data(va, new_size, 1);
 				return va;
 			}
@@ -433,6 +440,8 @@ void *realloc_block_FF(void* va, uint32 new_size)
 	else
 	{
 		uint32 diff = old_size - new_size;
+		//if diff <16 can be merged with next block if next block is free
+		//if diff >= 16 will be merged also
 		if (diff >= 16)
 		{
             set_block_data(va, new_size, 1);
