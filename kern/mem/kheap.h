32244374d3ee7d4ae3b0e2c28a5306216fb9b4d9
adham667 <adhamsaeed667@gmail.com>
2024-11-27 20:19:11 +0200

    fix commit

--- kern/mem/kheap.h
+++ kern/mem/kheap.h
@@ -43,6 +43,7 @@ int numOfKheapVACalls ;
 
 
 //TODO: [PROJECT'24.MS2 - #01] [1] KERNEL HEAP - add suitable code here
+////asdasdASDSDsda
 uint32 start, segBreak, rLimit;
 
 uint32 virtual_addresses_pages_num [NUM_OF_KHEAP_PAGES];
