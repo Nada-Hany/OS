052a456d8109537db8e3290f3e81dcd5847a4770
adham667 <adhamsaeed667@gmail.com>
2024-12-09 02:12:44 +0200

    testing semaphore phase 1

--- kern/trap/syscall.c
+++ kern/trap/syscall.c
@@ -367,25 +367,31 @@ void sys_block_env_sem(uint32 queue_ptr, uint32 lock_ptr) {
 	acquire_spinlock(&ProcessQueues.qlock);
 	//release semaphore lock
 	* lock = 0;
-
+	cprintf("released semaphore lock\n");
 	struct Env* env = get_cpu_proc();
 	if (queue != NULL && env != NULL) {
 		enqueue(queue, env);
 	}
-	sched_remove_ready(env);
+	//current process gets blocked some how
+//	sched_remove_ready(env);
 	env->env_status=ENV_BLOCKED;
+	cprintf("changed env status\n");
+	sched();
+	release_spinlock(&ProcessQueues.qlock);
+	cprintf("called sched\n");
 }
 
 void sys_release_env_sem(uint32 queue_ptr){
 	struct Env_Queue* queue = (struct Env_Queue*) queue_ptr;
 	struct Env* env=NULL;
-
+	acquire_spinlock(&ProcessQueues.qlock);
 	if(queue!=NULL){
+		cprintf("blocked queue size:%d", queue_size(queue));
 		env=dequeue(queue);
-		cprintf("after dequeue %d\n",env->env_id);
 		env->env_status=ENV_READY;
 		sched_insert_ready(env);
 	}
+//	release_spinlock(&ProcessQueues.qlock);
 
 }
 /*******************************/
